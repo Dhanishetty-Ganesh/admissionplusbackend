022c7d5695e7bcced7dff0e9cd0f173e3600337c
Dhanishetty-Ganesh <varmaganesh224@gmail.com>
2024-07-07 11:08:58 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,50 @@
+const express = require("express");
+const cors = require("cors");
+const {MongoClient} = require("mongodb");
+const dotenv = require("dotenv");
+
+const app = express();
+app.use(express.json())
+app.use(cors());
+dotenv.config();
+
+const uri = process.env.mongo_uri;
+const client = new MongoClient(uri);
+
+const dbname = "Institutelist";
+const collection_name = "Institutes";
+let accountsCollection;
+
+
+const connectToDatabase = async () => {
+  try {
+    await client.connect();
+    console.log(`Connected to the ${dbname} database`);
+    instituteCollection = client.db(dbname).collection(collection_name);
+  } catch (err) {
+    console.error(`Error connecting to the database : ${err}`);
+  }
+};
+
+app.get("/", (req,res) => {
+    res.send({success : "Hello World"})
+})
+
+app.get("/institutes", async (req,res) =>{
+    try{
+        await connectToDatabase()
+        const result = await instituteCollection.findOne({});
+        console.log(result);
+        res.send({success : "Institutes sent Successfully"})
+    }
+    catch(Err){
+        res.send({failure : `Error Occurred : ${Err}`})
+    }
+    finally {
+        await client.close()
+    }
+})
+
+app.listen(3001,() => {
+    console.log("Server is Running at http://localhost:3001")
+})
