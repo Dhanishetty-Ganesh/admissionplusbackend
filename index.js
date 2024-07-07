759439cc42f72470f038d42f891b7145ec99f2f7
Dhanishetty-Ganesh <varmaganesh224@gmail.com>
2024-07-07 16:57:15 +0530

    added api for institues

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 const express = require("express");
 const cors = require("cors");
-const {MongoClient} = require("mongodb");
+const { MongoClient } = require("mongodb");
 const dotenv = require("dotenv");
 
 const app = express();
-app.use(express.json())
+app.use(express.json());
 app.use(cors());
 dotenv.config();
 
@@ -13,8 +13,7 @@ const client = new MongoClient(uri);
 
 const dbname = "Institutelist";
 const collection_name = "Institutes";
-let accountsCollection;
-
+let instituteCollection;
 
 const connectToDatabase = async () => {
   try {
@@ -22,29 +21,39 @@ const connectToDatabase = async () => {
     console.log(`Connected to the ${dbname} database`);
     instituteCollection = client.db(dbname).collection(collection_name);
   } catch (err) {
-    console.error(`Error connecting to the database : ${err}`);
+    console.error(`Error connecting to the database: ${err}`);
   }
 };
 
-app.get("/", (req,res) => {
-    res.send({success : "Hello World"})
-})
+app.get("/", (req, res) => {
+  res.send({ success: "Hello World" });
+});
 
-app.get("/institutes", async (req,res) =>{
-    try{
-        await connectToDatabase()
-        const result = await instituteCollection.findOne({});
-        console.log(result);
-        res.send({success : "Institutes sent Successfully",result})
-    }
-    catch(Err){
-        res.send({failure : `Error Occurred : ${Err}`})
-    }
-    finally {
-        await client.close()
-    }
-})
+app.get("/institutes", async (req, res) => {
+  try {
+    await connectToDatabase();
+    const result = await instituteCollection.find({}).toArray();
+    res.send({ success: "Institutes sent Successfully", result });
+  } catch (Err) {
+    res.send({ failure: `Error Occurred: ${Err}` });
+  } finally {
+    await client.close();
+  }
+});
 
-app.listen(3001,() => {
-    console.log("Server is Running at http://localhost:3001")
-})
+app.post("/institutes", async (req, res) => {
+  try {
+    await connectToDatabase();
+    const newInstitute = req.body;
+    const result = await instituteCollection.insertOne(newInstitute);
+    res.send({ success: "Institute added successfully", result });
+  } catch (err) {
+    res.send({ failure: `Error occurred: ${err}` });
+  } finally {
+    await client.close();
+  }
+});
+
+app.listen(3001, () => {
+  console.log("Server is Running at http://localhost:3001");
+});
